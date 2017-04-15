52c5bddf62af3878f3eedc642fdcf742907b2fdd
高伟冬 <280823086@qq.com>
2017-04-15 23:15:33 +0800

    update

---
+++ src/main/java/number/Arith.java
@@ -0,0 +1,18 @@
+package number;
+
+/**
+ * Created by 高伟冬 on 2017/4/15.
+ * gao
+ * 23:11
+ * 星期六
+ */
+public class Arith {
+    public static void main(String[] args) {
+        float numder1=45.56f;
+        int numder2=152;
+        System.out.println("和为："+numder1+numder2);
+        System.out.println("差为："+(numder1-numder2));
+        System.out.println("积为："+(numder1*numder2));
+        System.out.println("商为："+(numder1/numder2));
+    }
+}
