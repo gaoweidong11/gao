90f5a0f5058453179122a51b7a11fd00a2c963d6
高伟冬 <280823086@qq.com>
2017-04-15 23:10:23 +0800

    update

---
+++ src/main/java/number/Eval.java
@@ -0,0 +1,17 @@
+package number;
+
+/**
+ * Created by 高伟冬 on 2017/4/15.
+ * gao
+ * 23:08
+ * 星期六
+ */
+public class Eval {
+    public static void main(String[] args) {
+        int a,b,c;
+        a=15;
+        c=b=a+4;
+        System.out.println("b的值："+b);
+        System.out.println("c的值："+c);
+    }
+}
---
+++ src/main/java/number/Val.java
@@ -0,0 +1,16 @@
+package number;
+
+/**
+ * Created by 高伟冬 on 2017/4/15.
+ * gao
+ * 22:28
+ * 星期六
+ */
+public class Val {
+    static int times = 3;
+
+    public static void main(String[] args) {
+        int times = 4;
+        System.out.println("times的值为：" + times);
+    }
+}
